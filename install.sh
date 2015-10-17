866672cedbbf584dc04a81cb728f212e4ddb2479
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-17 11:32:07 +0100

    Add an install.sh script

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-echo Download
+echo Downloading scripts...
+mkdir ~/.web > t.txt
+mkdir ~/.web/tubs > t.txt
+rm -rf t.txt
+echo Downloading build.sh
+echo
+curl --silent https://raw.githubusercontent.com/Gum-Joe/Web-OS-container/master/dist/build.sh -o ~/.web/tubs/build.sh
+echo Downloading run.sh...
+echo
+curl --silent https://raw.githubusercontent.com/Gum-Joe/Web-OS-container/master/dist/run.sh -o ~/.web/tubs/run.sh
