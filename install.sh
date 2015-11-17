bd452f7dcc414fbe2c283e3a28ef5e4223602ef6
Gum Joe <kishansambhi@hotmail.co.uk>
2015-11-17 20:00:21 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo Downloading scripts...
-mkdir ~/.web > t.txt
-mkdir ~/.web/tubs > t.txt
+mkdir ~/.boss > t.txt
+mkdir ~/.boss/tubs > t.txt
 rm -rf t.txt
 echo Downloading build.sh
 echo
-curl --silent https://raw.githubusercontent.com/Gum-Joe/Web-OS-container/master/dist/build.sh -o ~/.web/tubs/build.sh
+curl --silent https://raw.githubusercontent.com/Gum-Joe/Web-OS-container/master/dist/build.sh -o ~/.boss/tubs/build.sh
 echo Downloading run.sh...
 echo
-curl --silent https://raw.githubusercontent.com/Gum-Joe/Web-OS-container/master/dist/run.sh -o ~/.web/tubs/run.sh
+curl --silent https://raw.githubusercontent.com/Gum-Joe/Web-OS-container/master/dist/run.sh -o ~/.boss/tubs/run.sh
