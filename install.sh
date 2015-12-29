e6353bcd0b6a98aff7c5aa3881a875e946c7b36c
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2015-12-29 16:30:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,7 @@ mkdir ~/.jakhu/tubs > t.txt
 rm -rf t.txt
 echo Downloading build.sh
 echo
-<<<<<<< HEAD
-curl --silent https://raw.githubusercontent.com/Gum-Joe/jakhu-container/master/dist/build.sh -o ~/.web/tubs/build.sh
-echo Downloading run.sh...
-echo
-curl --silent https://raw.githubusercontent.com/Gum-Joe/jakhu-container/master/dist/run.sh -o ~/.web/tubs/run.sh
-=======
 curl --silent https://raw.githubusercontent.com/Gum-Joe/jakhu-container/master/dist/build.sh -o ~/.jakhu/tubs/build.sh
 echo Downloading run.sh...
 echo
 curl --silent https://raw.githubusercontent.com/Gum-Joe/jakhu-container/master/dist/run.sh -o ~/.jakhu/tubs/run.sh
->>>>>>> origin/master
